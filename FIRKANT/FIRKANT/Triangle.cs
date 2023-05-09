0b2dd7c6d32c5e6495f630d87f543320bb9eb42b
Janni <barstadjmj@gmail.com>
2023-05-09 08:05:05 +0200

    jmb

--- FIRKANT/FIRKANT/Triangle.cs
+++ FIRKANT/FIRKANT/Triangle.cs
@@ -4,10 +4,12 @@ namespace FIRKANT
 {
     internal class Triangle : Shape
     {
+        //Adding new variables: sideA, sideB and hypotenuse
         private double sideA;
         private double sideB;
         private double hypotenuse;
 
+        //name, 3 and hypotenuse is from the base
         public Triangle(string name, double sideA, double sideB, double hypotenuse) : base(name, 3, (int)hypotenuse)
         {
             this.sideA = sideA;
